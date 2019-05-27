5d0b6a8879f9b9b1c0716a468997f5dbba402ad0
Terje Torkelsen <tertork@gmail.com>
2019-05-27 23:12:24 +0200

    Added output variables

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "gateway_id" {
+    description = "The ID of the virtual network gateway."
+    value = azurerm_virtual_network_gateway.gw.id
+}
+
+output "fqdns" {
+    description = "List of the fqdn for gateway. Will return 2 for active_active mode and 1 otherwise"
+    value = flatten([[azurerm_public_ip.gw.fqdn], var.active_active ? [azurerm_public_ip.gw_aa[0].fqdn] : []])
+}
