b92f4e69c17078a732a90ebd1c4ef269c28fe624
Yngve Hansen <yngve.hansen2@avinor.no>
2022-01-19 11:47:46 +0100

    Upgrade azurerm provider to v2.92.0

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.55.0"
+      version = "~> 2.92.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "~> 3.1.0"
     }
   }
